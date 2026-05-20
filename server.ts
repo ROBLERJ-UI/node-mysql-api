004eec276acc63f55b755bb8ac41235018819609
ROBLERJ-UI <rjroble2921@gmail.com>
2026-05-21 04:11:08 +0800

    Changed the server.ts

--- server.ts
+++ server.ts
@@ -4,17 +4,40 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import errorHandler from './_middleware/error-handler';
 import db, { initialize as initializeDb } from './_helpers/db';
-import swaggerUi from 'swagger-ui-express';
-import swaggerUiDist from 'swagger-ui-dist';
-import YAML from 'yamljs';
 import path from 'path';
 
 const app = express();
-const swaggerDocument = YAML.load(path.resolve(process.cwd(), 'swagger.yaml'));
-const swaggerUiAssetPath = swaggerUiDist.getAbsoluteFSPath();
 
-app.use('/api-docs', express.static(swaggerUiAssetPath, { index: false }));
-app.get(['/api-docs', '/api-docs/'], swaggerUi.setup(swaggerDocument));
+app.get('/swagger.yaml', (req, res) => {
+  res.sendFile(path.resolve(process.cwd(), 'swagger.yaml'));
+});
+
+app.get(['/api-docs', '/api-docs/'], (req, res) => {
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Swagger UI</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = function() {
+      SwaggerUIBundle({
+        url: '/swagger.yaml',
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+        layout: 'StandaloneLayout'
+      });
+    };
+  </script>
+</body>
+</html>`);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());

e03f02a26cdc14c57293ac98e489c620a767fc55
ROBLERJ-UI <rjroble2921@gmail.com>
2026-05-21 13:43:10 +0800

    Fix reset password email to always include link

--- accounts/account.service.ts
+++ accounts/account.service.ts
@@ -259,14 +259,12 @@ async function sendAlreadyRegisteredEmail(email: any, origin: any) {
 
 async function sendPasswordResetEmail(account: any, origin: any) {
     let message;
-    if (origin) {
-        const resetUrl = `${origin}/account/reset-password?token=${account.resetToken}`;
-        message = `<p>Please click the below link to reset your password, the link will be valid for 1 day:</p>
-                   <p><a href="${resetUrl}">${resetUrl}</a></p>`;
-    } else {
-        message = `<p>Please use the below token to reset your password with the <code>/account/reset-password</code> api route:</p>
-                   <p><code>${account.resetToken}</code></p>`;
-    }
+    const resetUrl = origin 
+        ? `${origin}/account/reset-password?token=${account.resetToken}`
+        : `https://ipt-2026-frontend-ten.vercel.app/account/reset-password?token=${account.resetToken}`;
+    
+    message = `<p>Please click the below link to reset your password, the link will be valid for 1 day:</p>
+               <p><a href="${resetUrl}">${resetUrl}</a></p>`;
 
     await sendEmail({
         to: account.email,
